e5ea2f28a341149b1df60475fcd9c0fcde928641
Jeroen Heemskerk <41477180+JeroenHeemskerk@users.noreply.github.com>
2022-03-01 11:24:57 +0100

    added comments

--- src/main/java/net/haenkos/Controller.java
+++ src/main/java/net/haenkos/Controller.java
@@ -4,7 +4,7 @@ public class Controller {
     Logger logger;
     Prompter prompter;
 
-    Controller() {
+    /* JH TIP: Maak methoden (dus ook de constructor) 'public' of 'private' maar laat het niet weg */ Controller(/* JH TIP: Maak van de logger en de prompter een 'interface', en geef deze mee aan de constructor voor dependency injectie */) {
         logger = new Logger();
         prompter = new Prompter();
     }
@@ -12,18 +12,18 @@ public class Controller {
     public void newLogin() throws EndProgramException {
         String serNum = prompter.promptServiceNumber();
         if(logger.isLoggedIn(serNum)) {
-            System.out.println("already logged in. A good memory is essential for a spy.\n");
+            System.out.println("already logged in. A good memory is essential for a spy.\n"); /* JH TIP: Maak hiervoor kleine functie in de Prompter of een Displayer class */
         } else if (logger.isBlackListed(serNum)) {
-            System.out.println("You've tried this once. We are not falling for it: you are on the blacklist!\n");
+            System.out.println("You've tried this once. We are not falling for it: you are on the blacklist!\n"); /* JH TIP: Maak hiervoor kleine functie in de Prompter of een Displayer class */
         } else {
             if (prompter.promptPassfrase())
             {
                 logger.logIn(serNum);
-                System.out.printf("Welcome agent %s!\n\n", serNum);
+                System.out.printf("Welcome agent %s!\n\n", serNum); /* JH TIP: Maak hiervoor kleine functie in de Prompter of een Displayer class */
             } else {
                 logger.blackList(serNum);
-                System.out.println("Alarm has been raised. Please remain where you are, security forces are en route for pickup.\n");
-                System.out.println("Also you are now blacklisted!\n");
+                System.out.println("Alarm has been raised. Please remain where you are, security forces are en route for pickup.\n"); /* JH TIP: Maak hiervoor kleine functie in de Prompter of een Displayer class */
+                System.out.println("Also you are now blacklisted!\n"); 
             }
         }
     }
